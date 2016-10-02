1d310e10fbecf95a4753e1b20bbaf7c7943df661
Christian Loosli <loosli.christian@gmail.com>
2016-10-02 11:56:30 +0200

    Working on UI and defined orderBy variable in the controller

--- script.js
+++ script.js
@@ -25,6 +25,7 @@
 
     $scope.username = "angular";
     $scope.message = "GitHub Viewer";
+    $scope.repoSortOrder = "-stargazers_count";
   };
 
   app.controller('MainController', ["$scope", "$http", MainController]);
