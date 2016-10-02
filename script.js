fa0f9b990342c1e9d83c4b841dbafd93745341b0
Christian Loosli <loosli.christian@gmail.com>
2016-10-02 11:48:17 +0200

    Added MainController

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-// Code goes here
+var app = angular.module('app', []);
 
+
+var MainController = function($scope) {
+
+  var person = {
+    firstName: "Scott",
+    lastName: "Allen",
+    imageSrc: "https://unsplash.it/100/100?image=22"
+  }
+
+  $scope.message = "Hello, Angular!";
+  $scope.person = person;
+};
+
+var PersonCtrl = function($scope, $http) {
+  var promise = $http.get("user-1783.json");
+  promise.then(function(response){
+    $scope.user = response.data;
+  })
+}
+
+app.controller('MainController', MainController);
+app.controller('PersonController', PersonCtrl);
