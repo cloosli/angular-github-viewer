6ca29321f868549bf1328227df005260caee4d52
Christian Loosli <loosli.christian@gmail.com>
2016-10-03 17:03:23 +0200

    Added new User- and RepoController; Added new router service

---
+++ app.js
@@ -0,0 +1,25 @@
+(function() {
+
+  var app = angular.module("githubViewer", ["ngRoute"]);
+
+  app.config(['$locationProvider', '$routeProvider',
+    function config($locationProvider, $routeProvider) {
+      $locationProvider.hashPrefix('!');
+      $routeProvider
+        .when("/main", {
+          templateUrl: "main.html",
+          controller: "MainController"
+        })
+        .when("/user/:username",{
+          templateUrl: "user.html",
+          controller: "UserController"
+        })
+        .when("/repo/:username/:reponame",{
+          templateUrl: "repo.html",
+          controller: "RepoController"
+        })
+        .otherwise('/main');
+    }
+  ]);
+
+}());
--- github.js
+++ github.js
@@ -3,6 +3,7 @@
   var github = function($http) {
 
     var getUser = function(username) {
+      console.log("https://api.github.com/users/" + username);
       return $http.get("https://api.github.com/users/" + username)
         .then(function(response) {
           return response.data;
@@ -10,15 +11,32 @@
     };
 
     var getRepos = function(user) {
+      console.log(user.repos_url);
       return $http.get(user.repos_url)
         .then(function(response) {
           return response.data;
         });
     };
 
+    var getRepoDetails = function(username, reponame) {
+      var repo;
+      var repoUrl = "https://api.github.com/repos/" + username + "/" + reponame;
+      console.log(repoUrl);
+      return $http.get(repoUrl)
+        .then(function(response) {
+          repo = response.data;
+          return $http.get(repoUrl + "/contributors");
+        })
+        .then(function(response) {
+          repo.contributors = response.data;
+          return repo;
+        });
+    };
+
     return {
       getUser: getUser,
-      getRepos: getRepos
+      getRepos: getRepos,
+      getRepoDetails: getRepoDetails
     };
   };
 
